8a59e1981f2377aa066ed2c0229f51f27d2617cb
Luis Rodriguez <luishrt1997@gmail.com>
2019-11-04 20:15:44 -0800

    Final

---
+++ lib/src/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:booking_app/src/screens/welcome.dart';
+import 'package:booking_app/src/screens/home.dart';
+
+void main() => runApp(MaterialApp(
+      home: Welcome_screen(),
+    ));
